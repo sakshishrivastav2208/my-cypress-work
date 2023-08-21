aa655b6136f41630c0a5721006d15f9ba9fcce97
sakshishrivastav2208 <sakshishrivastav2208@gmail.com>
2023-08-21 23:46:19 +0530

    added few more test cases of practice

--- project/cypress.config.js
+++ project/cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
-
+const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on("task", { downloadFile });
+
       // implement node event listeners here
       //pageLoadTimeout: 4000;
     },
--- project/cypress/support/index.js
+++ project/cypress/support/index.js
@@ -1,3 +1,10 @@
 Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
 });
+
+const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
+module.exports = (on, config) => {
+  on("task", { downloadFile });
+};
+
+import "cypress-real-events/commands";
